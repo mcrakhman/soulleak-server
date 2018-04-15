aa7033da918e2d16507895242a0ec65a84453ad0
Mikhail Rakhmanov <mcrakhman@yandex-team.ru>
2018-04-15 15:34:03 +0300

    Rewritten image and gif handling logic

--- src/soulleak/handler.clj
+++ src/soulleak/handler.clj
@@ -1,5 +1,6 @@
 (ns soulleak.handler
   (:require [compojure.core :refer :all]
+            [soulleak.session :as session]
             [clojure.java.io :as io]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.util.response :refer [content-type response resource-response]]
@@ -9,114 +10,22 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
+;; Constants
+
 (def site-url "http://localhost:3000/")
 
-(def resources-images-path "resources/public")
-(def resources-audio-path "resources/audio")
-(def session-ids [])
-(def most-recent-session 0)
-(def total-images-count (count (list-dir resources-images-path)))
-(def number-of-images-in-pack 4)
+;; Helpers
 
+(defn string-to-boolean [string] (if (= string "true") true false))
 
-(defn fill-default-images-array [session-id]
-  (def session-image-map 
-    (assoc
-    session-image-map
-    (keyword (str session-id))
-    (into [] (range 0 total-images-count)))))
+(defn create-new-session-response [] (response {:session-id (session/create-session)}))
 
-(defn create-session []
-  (def most-recent-session (+ most-recent-session 1))
-  (def session-ids (conj session-ids most-recent-session))
-  (println "Created session")
-  (fill-default-images-array most-recent-session)
-  most-recent-session)
-
-(def session-image-map {})
-
-(defn parse-int [string] (-> string Double/parseDouble long))
-
-(defn remove-image-from-session [session-id number]
-  (let [session-index (keyword (str session-id))
-        previous-session-array (session-index session-image-map)]
-  (def session-image-map
-      (assoc
-      session-image-map
-      session-index
-      (into [] (filter #(not= number %) previous-session-array))))))
-
-(defn remove-images-from-session [session-id numbers]
-  (let [session-index (keyword (str session-id))
-        previous-session-array (session-index session-image-map)]
-  (def session-image-map
-      (assoc
-      session-image-map
-      session-index
-      (into [] (filter #(not (.contains numbers %)) previous-session-array))))))
-      
-(defn add-image-to-session [session-id number]
-  (let [session-index (keyword (str session-id))
-        previous-session-array (session-index session-image-map)]
-  (def session-image-map
-      (assoc
-      session-image-map
-      session-index
-      (into [] (conj previous-session-array number))))))
-
-(defn remove-session [n]
-  (def session-ids (filter #(not= n %) session-ids)))
-
-(defn file-in-folder [folder index]
-  (-> (list-dir folder)
-      (nth index)
-      (.getName)))
-
-(defn files-in-folder [folder indices]
-  (let [filepaths (list-dir folder)]
-    (map #(.getName %) (map #(nth filepaths %) indices))))
-
-(defn get-image-ratio [filename]
-  (let [path (str "resources/public/" filename)]
-    (with-open [r (java.io.FileInputStream. path)]
-      (let [image (javax.imageio.ImageIO/read r)]
-        (/ (.getWidth image) (.getHeight image))))))
-    
-(defn jpg-resource [filename]
-  (-> (response (io/file (str "resources/public/" filename)))
-      (content-type "image/jpg")))
- 
-(defn image-url [filename]
-  (str site-url "images/" filename))
-
-(defn swap [v i1 i2] (assoc v i2 (v i1) i1 (v i2)))
-
-(defn generate-random-numbers [max amount]
-  (let [random-array (into [] (range 0 max))]
-    (loop [result []
-           current-amount (min max amount)
-           current-array random-array]
-      (if (= current-amount 0)
-          result
-          (let [array-count (count current-array)
-                random (rand-int (- array-count (count result)))]
-            (recur (conj result (nth current-array random))
-                   (dec current-amount)
-                   (swap current-array random (- array-count (count result) 1))))))))
-
-(defn handle-images-pack-request [session-id amount]
-  (if (.contains session-ids session-id)
-    (let [session-index (keyword (str session-id))
-          previous-session-array (session-index session-image-map)
-          random-numbers (generate-random-numbers (count previous-session-array) amount)
-          random-file-numbers (map #(nth previous-session-array %) random-numbers)
-          random-files (filter #(or (string/ends-with? % ".jpg") (string/ends-with? % ".gif")) (files-in-folder resources-images-path random-file-numbers))
-          random-file-map (map #(assoc {} :file % :ratio (get-image-ratio %)) random-files)]
-            (remove-images-from-session session-id random-file-numbers)
-            (if (<= (count previous-session-array) (* amount 2))
-              (fill-default-images-array session-id)) 
-            (response {:image-url random-file-map}))
-    (response {:error "Image not found"})))
+(defn handle-image-request [session-id needs-gif]
+  (if (not (session/is-valid-session-id? session-id)) 
+    (response {:error "Image not found"})
+    (-> (session/take-images-and-gifs session-id needs-gif)
+        ((partial assoc {} :image-url))
+        (response))))
 
 (defn allow-cross-origin  
   "middleware function to allow crosss origin"  
@@ -126,11 +35,14 @@
     (assoc-in response [:headers "Access-Control-Allow-Origin"]  
           "*"))))    
 
+;; Routes
+
 (defroutes app-routes
-  (POST "/new-session" [] (response {:session-id (str (create-session))}))
-  (GET "/image-pack/:session-id" [session-id] (handle-images-pack-request (parse-int session-id) number-of-images-in-pack))
-  ;(GET "/images/:name" [name] (jpg-resource name))
+  (POST "/new-session" [] (create-new-session-response))
+  (GET "/image-pack/:session-id" [session-id needs-gif] (handle-image-request session-id (string-to-boolean needs-gif)))
   (route/resources "/images/")
+  (route/resources "/gifs/")
+  (route/resources "/audio/")
   (route/not-found "Not Found")) 
 
 (def app 
---
+++ src/soulleak/session.clj
@@ -0,0 +1,136 @@
+(ns soulleak.session
+    (:require [compojure.core :refer :all]
+              [clojure.java.io :as io]
+              [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+              [ring.util.response :refer [content-type response resource-response]]
+              [ring.middleware.resource :refer [wrap-resource]]
+              [me.raynes.fs :refer :all]
+              [clojure.string :as string]
+              [compojure.route :as route]
+              [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+
+;; Constants
+
+(def resources-path "resources/public")
+
+(def image-path "resources/public/images")
+
+(def gif-path "resources/public/gifs")
+
+(def audio-path "resources/public/audio")
+
+(def audio-key :audio)
+(def image-key :images)
+(def gif-key :gifs)
+
+(def image-portion-count 4)
+(def gif-portion-count 1)
+
+;; Atoms
+
+(def session-data (atom {}))
+
+;; General helpers
+
+(defn choose-last [x y] y)
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn randomized-range [min max] 
+    (-> [] 
+        (into (range min max))
+        (shuffle)))
+
+(defn insert-at [pos vec item] 
+    (apply conj (subvec vec 0 pos) item (subvec vec pos)))
+
+(defn merge-random [vector element]
+    (-> (count vector)
+        (rand-int)
+        (insert-at vector element)))
+
+(defn name-replacement [key] 
+    (-> (str key)
+        (string/split #":")
+        (last)))
+
+;; Database operations
+
+(defn add-nested [key-array doc] (swap! session-data update-in key-array conj doc) doc)
+(defn replace-nested [key-array doc] (swap! session-data update-in key-array choose-last doc) doc)
+
+(defn add-reshuffled-array [session-id key n-max]
+    (-> []
+        (conj (keyword session-id) key)
+        (replace-nested (randomized-range 0 n-max))))
+
+;; File helpers
+
+(defn get-image-ratio [filepath]
+    (with-open [r (java.io.FileInputStream. filepath)]
+        (let [image (javax.imageio.ImageIO/read r)]
+            { :width (.getWidth image) :height (.getHeight image) }
+        )))
+
+(defn file-in-folder [folder index]
+    (-> (list-dir folder)
+        (nth index)
+        (.getName)))
+
+(defn files-in-folder [folder indices]
+    (let [filepaths (list-dir folder)
+          last-folder-name (last (string/split folder #"/"))]
+        (into [] (map #(str last-folder-name "/" (.getName %)) (map #(nth filepaths %) indices)))))
+
+(defn total-count-in-path [path] (count (list-dir path)))
+
+(defn total-images [] (total-count-in-path image-path))
+(defn total-gifs [] (total-count-in-path gif-path))
+(defn total-audio [] (total-count-in-path audio-path))
+
+(def all-folder-keys [audio-key gif-key image-key])
+(def all-folder-file-amount-map { audio-key total-audio image-key total-images gif-key total-gifs })
+
+(defn reshuffle-images [session-id] (add-reshuffled-array session-id image-key (total-images)))
+(defn reshuffle-audio [session-id] (add-reshuffled-array session-id audio-key (total-audio)))
+(defn reshuffle-gifs [session-id] (add-reshuffled-array session-id gif-key (total-gifs)))
+
+;; Sessions
+
+(defn create-session []
+    (let [session-id (uuid)]
+        (doall 
+            (map #(add-reshuffled-array session-id % ((% all-folder-file-amount-map))) all-folder-keys))
+        session-id))
+
+(defn all-session-ids [] (map name-replacement (keys @session-data)))
+
+(defn is-valid-session-id? [session-id] (.contains (all-session-ids) session-id))
+
+(defn take-reshuffle [amount session-id key]
+    (let [initial-array (key ((keyword session-id) @session-data))
+          previous-array (if (< (count initial-array) (* amount 2))
+                            (add-reshuffled-array session-id key ((key all-folder-file-amount-map)))
+                            initial-array)
+          array-count (count previous-array)
+          replaced-array (subvec previous-array 0 (- array-count amount))
+          returned-array (take-last amount previous-array)]
+          (do (replace-nested [(keyword session-id) key] replaced-array)
+            (into [] (take-last amount previous-array)))))
+
+(defn take-images [session-id]
+    (files-in-folder image-path (into [] (take-reshuffle image-portion-count session-id image-key))))     
+            
+(defn add-gif-if-needed [array needs-gif? session-id] 
+    (if needs-gif? 
+        (apply conj array
+            (files-in-folder gif-path (take-reshuffle gif-portion-count session-id gif-key)))
+        array))
+
+(defn add-dimensions-data [array]
+    (map #(assoc {} :file % :data (get-image-ratio (str resources-path "/" %))) array))
+
+(defn take-images-and-gifs [session-id needs-gif?]
+    (-> (take-images session-id)
+        (add-gif-if-needed needs-gif? session-id)
+        (add-dimensions-data)))
